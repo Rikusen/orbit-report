d7db2d5b43617085aa46078a3b19ab1f3732584b
Rikusen <arigatoriku@aol.com>
2020-08-11 20:39:16 -0500

    Completed Assignment... Somehow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];
+  displayList: Satellite[];
+
+constructor(){
+  this.sourceList = [];
+  let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+  this.displayList = [];
+
+  window.fetch(satellitesUrl).then(function(response) {
+    response.json().then(function(data) {
+
+      let fetchedSatellites = data.satellites;
+        for (let i = 0; i < fetchedSatellites.length; i++){
+          let satellite = new Satellite (fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          this.sourceList.push(satellite);
+        }
+        this.displayList = this.sourceList.slice(0);
+      }.bind(this));
+    }.bind(this));
+  }
+
+  search (searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      let orbitType = this.sourceList[i].orbitType.toLowerCase();
+      let type = this.sourceList[i].type.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+      } else if (orbitType.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+      } else if (type.indexOf(searchTerm) >= 0) {
+      matchingSatellites.push(this.sourceList[i]);
+      }  
+    }
+    this.displayList = matchingSatellites;
+  }
 }
---
+++ src/app/satellite.ts
@@ -0,0 +1,25 @@
+export class Satellite {
+
+name: string;
+orbitType: string;
+type: string;
+operational: boolean;
+launchDate: string;
+
+    constructor (name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        this.name = name;
+        this.orbitType = orbitType;
+        this.type = type;
+        this.operational = operational;
+        this.launchDate = launchDate;
+    }
+
+    shouldShowWarning(): boolean {
+        let item = this.type.toLowerCase();
+        if (item === "space debris"){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
